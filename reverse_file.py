8cb2c58696eb570286a7194c1ead361b584acd2a
Janetta Dunbar <janettadunbar@Janettas-MacBook-Air.local>
2019-02-18 19:10:47 -0800

    created contains.py file

--- reverse_file.py
+++ reverse_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.7
 
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Read a file in reverse')
 parser.add_argument('filename', help='the file to read')
@@ -9,10 +10,20 @@ parser.add_argument('--limit', '-l', type=int,
 parser.add_argument('--version', '-v', action='version',
                     version='%(prog)s 1.0')
 args = parser.parse_args()
-print(args)
 
-# build the parser
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+    sys.exit(2)
+else:
+    with open(args.filename) as f:
+        lines = f.readlines()
+        lines.reverse()
 
-# parse the arguments
+    if args.limit:
+        lines = lines[:args.limit]
 
-# read the file, revers the contents and print
+    for line in lines:
+        print(line.strip()[::-1])
