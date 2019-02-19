85eef0aba5e9e2697ee6b39de5d8c5d7ee8cd528
Janetta Dunbar <janettadunbar@Janettas-MacBook-Air.local>
2019-02-18 17:53:09 -0800

    created param_echo and reverse_file scripts

---
+++ reverse_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.7
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Read a file in reverse')
+parser.add_argument('filename', help='the file to read')
+parser.add_argument('--limit', '-l', type=int,
+                    help='the number of lines to read')
+parser.add_argument('--version', '-v', action='version',
+                    version='%(prog)s 1.0')
+args = parser.parse_args()
+print(args)
+
+# build the parser
+
+# parse the arguments
+
+# read the file, revers the contents and print
